0b0954e454118b9c47f0160c55e2dd87f90abf89
PechPanupong <51985680+PechPanupong@users.noreply.github.com>
2023-05-29 14:35:52 +0700

    rename, refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import player1 from "./assets/player2.png";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "./text-style.css";
-import { Splide, SplideTrack, SplideSlide } from "@splidejs/react-splide";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
-import AliceCarousel from "react-alice-carousel";
 function App() {
-  const [count, setCount] = useState(0);
-
   const atletes = [
     {
       number: "01",
-      numberColor: "",
-      underline: "bg-primary",
       background: "",
-      textColor: "black",
       title: "CONNECTION",
       content:
         "Connect with coaches directly, you can ping coaches to view profile.",
     },
     {
       number: "02",
-      numberColor: "",
-      underline: "bg-primary",
       background: "bg-secondary",
-      textColor: "black",
       title: "COLLABORATION",
       content:
         "Work with other student athletes to  increase visability. When you share and like other players videos it will increase your visability as a player. This is the team work aspect to Surface 1.",
     },
     {
       number: "03",
-      numberColor: "",
-      underline: "bg-white",
       background: "bg-primary",
-      textColor: "white",
       title: "GROWTH",
       content:
         "Resources and tools for you to get better as a student Athelte. Access to training classes, tutor sessions, etc ",
@@ -46,18 +30,13 @@ function App() {
   const players = [
     {
       number: "01",
-      numberColor: "",
-      underline: "bg-primary",
       background: "",
-      textColor: "black",
       title: "CONNECTION",
       content:
         "Connect with talented athlete directly, you can watch their skills through video showreels directly from Surface 1.",
     },
     {
       number: "02",
-      numberColor: "",
-      underline: "bg-primary",
       background: "bg-secondary",
       textColor: "black",
       title: "COLLABORATION",
@@ -66,9 +45,6 @@ function App() {
     },
     {
       number: "03",
-      numberColor: "primary",
-      underline: "bg-white",
-      textColor: "white",
       background: "bg-third",
       title: "GROWTH",
       content: "Save your time, recruit proper athlets for your team.",
@@ -77,31 +53,34 @@ function App() {
 
   return (
     <>
-      <div id="section1" className="container">
+      <div className="container">
         <section id="section1">
           <img className="player1"></img>
 
           <div className="header-container">
-            <div className="header-box header">ATHLETS</div>
+            <div className="title-box title">ATHLETS</div>
           </div>
 
           {atletes.map((item, index) => (
-            <div
-              key={index}
-              className={`${item.background} content-container `}
-            >
-              <div className="content-box">
-                <div className="content-header-box">
+            <div key={index} className={`${item.background} content-row `}>
+              <div className="content-container">
+                <div className="content-title-box">
                   <div className="number-container ">
                     <div className="number-text">{item.number}</div>
                     <div
-                      className={`${item.underline} number-text-underline`}
+                      className={`${
+                        index == atletes.length - 1 ? "bg-white" : "bg-primary"
+                      } number-text-underline`}
                     ></div>
                   </div>
-                  <div className="content-header-text">{item.title}</div>
+                  <div className="content-title-text">{item.title}</div>
                 </div>
                 <div className="content-text-box">
-                  <div className={`${item.textColor} content-text`}>
+                  <div
+                    className={`${
+                      index == atletes.length - 1 ? "white" : "black"
+                    } content-text`}
+                  >
                     {item.content}
                   </div>
                 </div>
@@ -118,18 +97,16 @@ function App() {
               {atletes.map((item, index) => (
                 <SplideSlide>
                   <div key={index}>
-                    <div className="content-box">
-                      <div className="content-header-box">
+                    <div className="content-container">
+                      <div className="content-title-box">
                         <div className="number-container ">
                           <div className="number-text">{item.number}</div>
-                          <div
-                            className={`bg-primary number-text-underline`}
-                          ></div>
+                          <div className="bg-primary number-text-underline"></div>
                         </div>
-                        <div className="content-header-text">{item.title}</div>
+                        <div className="content-title-text">{item.title}</div>
                       </div>
                       <div className="content-text-box">
-                        <div className={`content-text`}>{item.content}</div>
+                        <div className="content-text">{item.content}</div>
                       </div>
                     </div>
                   </div>
@@ -140,36 +117,41 @@ function App() {
         </section>
 
         <section id="section2" className="section-padding">
-          <div className="w-full">
-            <div className="header-container">
-              <div className="header-box header">PLAYERS</div>
-            </div>
-            {players.map((item, index) => (
-              <div
-                key={index}
-                className={`${item.background} content-container`}
-              >
-                <div className="content-box">
-                  <div className="content-header-box">
-                    <div className="number-container">
-                      <div className={` ${item.numberColor} number-text`}>
-                        {item.number}
-                      </div>
-                      <div
-                        className={`${item.underline} number-text-underline`}
-                      ></div>
+          <div className="header-container">
+            <div className="title-box title">PLAYERS</div>
+          </div>
+          {players.map((item, index) => (
+            <div key={index} className={`${item.background} content-row`}>
+              <div className="content-container">
+                <div className="content-title-box">
+                  <div className="number-container">
+                    <div
+                      className={` 
+                      ${index == atletes.length - 1 ? "primary" : "black"}
+                      number-text`}
+                    >
+                      {item.number}
                     </div>
-                    <div className="content-header-text">{item.title}</div>
+                    <div
+                      className={`${
+                        index == atletes.length - 1 ? "bg-white" : "bg-primary"
+                      } number-text-underline`}
+                    ></div>
                   </div>
-                  <div className="content-text-box">
-                    <div className={`${item.textColor} content-text`}>
-                      {item.content}
-                    </div>
+                  <div className="content-title-text">{item.title}</div>
+                </div>
+                <div className="content-text-box">
+                  <div
+                    className={`${
+                      index == atletes.length - 1 ? "white" : "black"
+                    } content-text`}
+                  >
+                    {item.content}
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
           <img className="player2"></img>
           <div className="slide-container bg-secondary">
             <Splide
@@ -180,15 +162,15 @@ function App() {
               {players.map((item, index) => (
                 <SplideSlide>
                   <div key={index}>
-                    <div className="content-box">
-                      <div className="content-header-box">
+                    <div className="content-container">
+                      <div className="content-title-box">
                         <div className="number-container ">
                           <div className="number-text">{item.number}</div>
                           <div
                             className={`bg-primary number-text-underline`}
                           ></div>
                         </div>
-                        <div className="content-header-text">{item.title}</div>
+                        <div className="content-title-text">{item.title}</div>
                       </div>
                       <div className="content-text-box">
                         <div className={`content-text`}>{item.content}</div>
